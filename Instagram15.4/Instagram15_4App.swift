43a303c642fe90f0a34e79bd77539206cf58a11c
승찬 <anstmdcks12@naver.com>
2023-05-30 13:09:12 +0900

    Initial Commit

---
+++ Instagram15.4/Instagram15_4App.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram15_4App.swift
+//  Instagram15.4
+//
+//  Created by 승찬 on 2023/05/30.
+//
+
+import SwiftUI
+
+@main
+struct Instagram15_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
